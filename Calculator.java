f62c6a0c74e56c205dc26a014930b656733ba9f1
Vaib215 <91018916+Vaib215@users.noreply.github.com>
2021-09-19 21:06:47 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main{
+     public static void main(String []args){
+        System.out.println("This code does mathematical operations on two numbers given by the user. Made by VAIB.");
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        a = sc.nextInt();
+        System.out.print("Enter Second Number : ");
+        b = sc.nextInt();
+        System.out.println("Enter Mathematical operator name. Type 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division and 5 for Remainder.");
+        c = sc.nextInt();
+        switch(c) {
+            case 1:
+                System.out.println(a+b);
+                break;
+            case 2:
+                System.out.println(a-b);
+                break;
+            case 3:
+                System.out.println(a*b);
+                break;
+            case 4:
+                System.out.println(a/b);
+                break;
+            case 5:
+                System.out.println(a%b);
+                break;
+            default:
+                System.out.println("Invalid operation");
+        }
+            System.out.println("Thanks for using the Code.");
+     }
+}
